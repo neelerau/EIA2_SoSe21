f83709a71ad669946e2f863e7f7f7cef364c7d82
neelerau <72865700+neelerau@users.noreply.github.com>
2021-03-27 18:36:31 +0100

    Update Aufgabe1.ts

--- Aufgabe1/Aufgabe1.ts
+++ Aufgabe1/Aufgabe1.ts
@@ -1,20 +1,31 @@
 namespace randomPoem {
-    let name: string [] = ["Harry ", "Hermine ", "Ron ", "Hagrid ", "Dumbledore ", "Snape "];
-    let action: string [] = ["liebt", "braut", "studiert", "hasst", "zaubert", "zerstört"];
-    let object: string [] = [" Zaubertränke", " den Grimm", " Lupin", " Hogwarts", " die Karte des Rumtreibers", " Dementoren"];
-    let sentence: string = "";
 
+    //Arrays mit den einzelnen Satzteilen
+    let name: string [] = ["Harry ", "Hermine ", "Ron ", "Hagrid ", "Dumbledore ", "Snape "];
+    let action: string [] = [" liebt", " braut", " studiert", " hasst", " zaubert", "zerstört"];
+    let object: string [] = [" Zaubertränke", " den Grimm", " Lupin", " Hogwarts", " die Karte des Rumtreibers", " Dementoren"];
+    
+    //leeres String, in welches die Satzteile angeordnet werden
+    let sentence: string = "";
+     
+    //console.log(name[], action[], object[]);
+
+
+    //Forschleife, welche die Laufvariable anhand der Länge des Arrays rückwärts durchzählt
     for (let index: number = 6; index <= object.length; index--) {
         if (index == 0) {
             break;
         }
+        //console.log(index);
         getVerse(name, action, object, sentence);
     }
-
+    
+    //In der Funktion werden die Werte zurückgegeben, Name als Präfix mit Unterstrick
     function getVerse (_name: string[], _action: string[], _object: string[], _sentence: string): string {
-
-    let randomName: number = Math.floor(Math.random() * _name.length);
-    _sentence += _name.splice(randomName, 1) + "";
+    //console.log("alohomara");
+    //Math.floor, rundet die Nachkommastelle auf
+    let randomName: number = Math.floor(Math.random() * _name.length); //Mit Math.random wird eine zufällioge Zahl erzeugt
+    _sentence += _name.splice(randomName, 1) + ""; //Mit splice wird ein Wort aus dem Array gezogen und in den Satz eingefügt
 
     let randomAction: number = Math.floor(Math.random() * _action.length);
     _sentence += _action.splice(randomAction, 1) + "";
